d6f90386cbc206d3bfb49431977fe02441153e20
Lorenzo Iannuzz <lorenzoi@how2code.io>
2017-10-07 06:42:25 -0300

    Added Magit. Oh and custom.el showed up.

---
+++ emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit with-editor which-key use-package rust-mode org-bullets org-babel-eval-in-repl monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
