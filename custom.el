5eb988362948302d23efde583cb9d27198dce5b1
Lorenzo Iannuzz <lorenzoi@how2code.io>
2017-10-08 06:57:19 -0300

    Added Some Fun Things

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-c-headers which-key use-package rust-mode org-bullets org-babel-eval-in-repl monokai-theme markdown-mode magit company))))
+    (hacker-typer which-key use-package rust-mode org-bullets org-babel-eval-in-repl monokai-theme markdown-mode magit company-c-headers))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
