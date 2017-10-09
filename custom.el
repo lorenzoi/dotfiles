380c36e832659d4db39212a007d1ee4dd35390ce
Lorenzo Iannuzz <lorenzoi@how2code.io>
2017-10-09 06:45:07 -0300

    Added God-Mode. More Info: http://chrisdone.com/posts/god-mode.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (hacker-typer which-key use-package rust-mode org-bullets org-babel-eval-in-repl monokai-theme markdown-mode magit company-c-headers))))
+    (god-mode which-key use-package rust-mode org-bullets org-babel-eval-in-repl monokai-theme markdown-mode magit hacker-typer company-c-headers))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
